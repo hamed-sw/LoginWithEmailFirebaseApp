6eaeebd0ff4ac17335d197e40e7a817f648f8e6f
Hamed <hamedullah@gmail.com>
2021-08-31 12:30:33 +0300

    Initial Commit

---
+++ LoginWithEmailFirebaseApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginWithEmailFirebaseApp
+//
+//  Created by Hamed Amiry on 31.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
